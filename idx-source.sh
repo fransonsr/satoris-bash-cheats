ffe0ae57b80e001fe49624ec81f16510590f917c
Scott Franson <fransonsr@familysearch.org>
2020-12-07 11:29:50 -0700

    Add branch command

--- idx-source.sh
+++ idx-source.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export IDX_SOURCE_VERSION=0.1.1
+
 #
 # Sets environment variables for other scripts. Principally,
 # WORKDIR which is the root directory for all git projects
@@ -20,9 +22,8 @@ export IDX_ALL="$IDX_PROJECTS"
 
 # Variables used for command completion
 export IDX_COMMAND_LAZY="api admin template-app swing super core domain db-migration discovery metric orchestration project statistic template user workflow mailbox"
-export IDX_COMMANDS="$IDX_ALL status update reset-master $IDX_COMMAND_LAZY"
+export IDX_COMMANDS="$IDX_ALL status update reset-master branch $IDX_COMMAND_LAZY"
 export IDX_OPTIONS="-h --help"
-export IDX_UPDATE_OPTIONS="-c --clean-merged"
 
 rootUsage() {
   cat <<-EOF
@@ -52,6 +53,7 @@ COMMANDS:
 
   status                check the git status of the repository
   update                update the repository
+  branch                report on the repository's branches
   reset-master          force a reset of the local master branch to the remote branch
 
 OPTIONS:
@@ -65,6 +67,7 @@ DEPENDENCIES:
   xmlstarlet            XML processing tool
   mvn                   Maven build tool
   docker                Container management
+  pcre                  Pearl-compatible expression language utilities
 
 ERRORS:
   If an error occurs the script will exit with '1' and IDX_ERROR will contain
@@ -73,18 +76,20 @@ ERRORS:
 EOF
 }
 
+cdToProject-usage() {
+  cat <<-EOF
+USAGE: idx <project>
+
+Change the current working directory to the root of the project.
+
+OPTIONS: <project>      name of the GitHub project
+EOF
+}
+
 cdToProject() {
   case "$1" in
     -h | --help)
-      rootUsage
-      cat <<-EOF
-  ========================================================================
-  USAGE: idx <project>
-
-  Change the current working directory to the root of the project.
-
-  OPTIONS: <project>      name of the GitHub project
-EOF
+      cdToProject-usage
       IDX_ERROR="cdToProject - command help"
       return
       ;;
@@ -99,19 +104,25 @@ EOF
     IDX_ERROR="cdToProject"
     return
   fi
-  cd "$WORKDIR/$PROJECT" || { IDX_ERROR="cdToProject"; echo "Failed to 'cd' to $WORKDIR/$PROJECT"; return 1;}
+  cd "$WORKDIR/$PROJECT" || {
+    IDX_ERROR="cdToProject"
+    echo "Failed to 'cd' to $WORKDIR/$PROJECT"
+    return 1
+  }
+}
+
+idx-status-usage() {
+  cat <<-EOF
+USAGE: idx status
+
+Report on the 'git-status' of the repository.
+EOF
 }
 
 idx-status() {
-    case "$1" in
+  case "$1" in
     -h | --help)
-      rootUsage
-      cat <<-EOF
-  ========================================================================
-  USAGE: idx status
-
-  Report on the 'git-status' of the repository.
-EOF
+      idx-status-usage
       IDX_ERROR="idx-status - command help"
       return
       ;;
@@ -119,48 +130,59 @@ EOF
       ;;
   esac
 
-  RESULT=$(git status)
-  PREFIX=
-  POSTFIX=
-  if [[ $RESULT != *"master"* ]]
-  then
-          PREFIX='\e[1;33m'
-          POSTFIX='\e[0m'
-  elif [[ $RESULT != *"up-to-date"* ]]
-  then
-          PREFIX='\e[1;34m'
-          POSTFIX='\e[0m'
+  local result=$(git status)
+  local prefix
+  local postfix=
+  if [[ $result != *"master"* ]]; then
+    prefix='\e[1;33m'
+    postfix='\e[0m'
+  elif [[ $result != *"up-to-date"* ]]; then
+    prefix='\e[1;34m'
+    postfix='\e[0m'
   fi
-  echo -e "$PREFIX$RESULT$POSTFIX"
+  echo -e "$prefix$result$postfix"
+}
+
+idx-update-usage() {
+  cat <<-EOF
+USAGE: idx update [[-c | --clean-merged]]
+
+Updates the 'master' branch of the repository. Checks out the 'master'
+branch if it is not currently checked out and performs a 'git pull' request.
+
+OPTIONS:
+-c | --clean-merged   removes local tracked branches that have been
+                      merged into 'master'.
+EOF
+}
+
+_idx-update() {
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W "-c --clean-merged" -- "$cur"))
 }
 
 idx-update() {
   local idx_status_clean_merged
 
-  case "$1" in
-    -h | --help)
-      rootUsage
-      cat <<-EOF
-  ===========================================================================
-  USAGE: idx update [[-c | --clean-merged]]
-
-  Updates the 'master' branch of the repository. Checks out the 'master'
-  branch if it is not currently checked out and performs a 'git pull' request.
-
-  OPTIONS:
-  -c | --clean-merged   removes local tracked branches that have been
-                        merged into 'master'.
-EOF
-      IDX_ERROR="idx-update - command help"
-      return
+  while [ $# -gt 0 ]; do
+    case "$1" in
+      -h | --help)
+        idx-update-usage
+        IDX_ERROR="idx-update - command help"
+        return
       ;;
     -c | --clean-merged)
       idx_status_clean_merged=true
       shift
       ;;
     *)
+      echo Option \'"$1"\' not recognized.
+      idx-update-usage
+      IDX_ERROR="idx-reset-master - options"
+      return
       ;;
-  esac
+    esac
+  done
 
   git checkout master
   git pull
@@ -174,22 +196,28 @@ EOF
   fi
 }
 
-idx-reset-master() {
-    case "$1" in
-    -h | --help)
-      rootUsage
-      cat <<-EOF
-  ===========================================================================
-  USAGE: idx reset-master
+idx-reset-master-usage() {
+  cat <<-EOF
+USAGE: idx reset-master
 
-  Force a deletion of the local master branch and recreate it from the remote
-  master branch. Useful if the local master is accidentally modified.
+Force a deletion of the local master branch and recreate it from the remote
+master branch. Useful if the local master is accidentally modified.
 EOF
+}
+
+idx-reset-master() {
+  case "$1" in
+    -h | --help)
+      idx-reset-master-usage
       IDX_ERROR="idx-reset-master - command help"
       return
-      ;;
-    *)
-      ;;
+    ;;
+  *)
+    echo Option \'"$1"\' not recognized.
+    idx-reset-master-usage
+    IDX_ERROR="idx-reset-master - options"
+    return
+    ;;
   esac
 
   local modified_count="$(git status --porcelain=2 | wc -l)"
@@ -206,10 +234,160 @@ EOF
   git branch --delete --force killme
 }
 
-idx-all() {
+idx-branch-usage() {
+  cat <<-EOF
+USAGE: idx branch [[-a | all] [-c | --current] [-m | --merged] [-n | --no-merged]
+                   [-r | --remotes] [-f | --filter-pr]]
+
+Report on the repository's branches.
+
+OPTIONS:
+  -a | --all          Report on all branches (local and remote).
+  -c | --current      Report on the current branch only.
+  -m | --merged       Report on branches merged into the current branch only.
+  -n | --no-merged    Report on branches not merged into the current branch only.
+  -r | --remotes      Report on remote branches.
+  -f | --filter-pr    Filter out local and remote 'pr' branches.
+EOF
+}
+
+_idx-branch() {
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W "-a --all -c --current -m --merged -n --no-merged -r --remotes -f --filter-pr" -- "$cur"))
+}
+
+idx-branch() {
+  local current=false
+  local filterpr=false
+  local options=()
+
+  while [ $# -gt 0 ]; do
     case "$1" in
+      -h | --help)
+        idx-branch-usage
+        IDX_ERROR="idx-reset-master - command help"
+        return
+        ;;
+      -a | --all)
+        options+=("--all")
+        shift
+        ;;
+      -c | --current)
+        current=true
+        shift
+        ;;
+      -m | --merged)
+        options+=("--merged")
+        shift
+        ;;
+      -n | --no-merged)
+        options+=("--no-merged")
+        shift
+        ;;
+      -r | --remotes)
+        options+=("--remotes")
+        shift
+        ;;
+      -f | --filter-pr)
+        filterpr=true
+        shift
+        ;;
+      *)
+        echo Option \'"$1"\' not recognized.
+        idx-branch-usage
+        IDX_ERROR="idx-reset-master - options"
+        return
+        ;;
+    esac
+  done
+
+  if $current; then
+    local prefix
+    local postfix
+    local current_branch
+    
+    current_branch="$(git branch | grep "\*" | cut -d" " -f2)"
+    if [[ "$current_branch" == "master" ]]; then
+      prefix='\e[1;32m'
+      postfix='\e[0m'
+    else
+      prefix='\e[1;31m'
+      postfix='\e[0m'
+    fi
+    echo -e $prefix$current_branch$postfix
+  elif $filterpr; then
+    git -P branch "${options[@]}" | grep -v -P -e "^\s*((remotes/)?origin/)?pr/\d+"
+  else
+    git -P branch "${options[@]}"
+  fi
+}
+
+_idx-all() {
+  local i=1 subcommand_index
+
+  while [[ $i -lt $COMP_CWORD ]]; do
+    local s="${COMP_WORDS[i]}"
+    case "$s" in
+      -h | --help)
+        subcommand_index=$i
+        break
+        ;;
+      status)
+        subcommand_index=$i
+        break
+        ;;
+      update)
+        subcommand_index=$i
+        break
+        ;;
+      branch)
+        subcommand_index=$i
+        break
+        ;;
+      reset-master)
+        subcommand_index=$i
+        break
+        ;;
+    esac
+    (( i++ ))
+  done
+
+  while [[ $subcommand_index -lt $COMP_CWORD ]]; do
+    local s="${COMP_WORDS[subcommand_index]}"
+    case "$s" in
+      -h | --help)
+        COMPREPLY=()
+        return
+        ;;
+      status)
+        _idx-status
+        return
+        ;;
+      update)
+        _idx-update
+        return
+        ;;
+      branch)
+        _idx-branch
+        return
+        ;;
+      reset-master)
+        COMPREPLY=()
+        return
+        ;;
+    esac
+    (( subcommand_index++ ))
+  done
+
+  local cur="${COMP_WORDS[COMP_CWORD]}"
+  COMPREPLY=($(compgen -W "-h --help status update branch reset-master" -- "$cur"))
+}
+
+idx-all() {
+  case "$1" in
     -h | --help)
       idx "$@"
+      shift
       return
       ;;
     *)
@@ -217,21 +395,31 @@ idx-all() {
   esac
 
   pushd . >/dev/null
-  for DIR in $IDX_ALL
-  do
+  for dir in $IDX_ALL; do
     echo ========================
-    echo PROJECT - "$DIR"
-    cd "$WORKDIR/$DIR" || { IDX_ERROR="idx-all"; echo "Failed to 'cd' to $WORKDIR/$PROJECT"; return 1;}
+    echo PROJECT - "$dir"
+    cd "$WORKDIR/$dir" || {
+      IDX_ERROR="idx-all"
+      echo "Failed to 'cd' to $WORKDIR/$PROJECT"
+      return 1
+    }
     idx "$@"
+    if [ ! -z "$IDX_ERROR" ]; then
+      break
+    fi
   done
-  popd >/dev/null || { IDX_ERROR="idx-all"; echo "Failed to return to starting directory"; return 1;}
+  popd >/dev/null || {
+    IDX_ERROR="idx-all"
+    echo "Failed to return to starting directory"
+    return 1
+  }
 
 }
 
 idx() {
   local project
+  local helpargs
   unset -v IDX_ERROR
-  unset -v HELPARGS
 
   # validate there is a valid WORKDIR
   if [ ! -d "$WORKDIR" ]; then
@@ -243,7 +431,7 @@ idx() {
   while [ $# -ge 0 ]; do
     case "$1" in
     -h | --help)
-      HELPARGS+="$1"
+      helpargs+="$1"
       shift
       ;;
     all)
@@ -342,23 +530,28 @@ idx() {
       local argOne=${project:-$1}
       # regardless, shift the argument list
       shift
-      cdToProject ${HELPARGS:+"$HELPARGS"} "$argOne" "$@"
-      break;
+      cdToProject ${helpargs:+"$helpargs"} "$argOne" "$@"
+      break
       ;;
     status)
       shift
-      idx-status ${HELPARGS:+"$HELPARGS"} "$@"
-      break;
+      idx-status ${helpargs:+"$helpargs"} "$@"
+      break
       ;;
     update)
       shift
-      idx-update ${HELPARGS:+"$HELPARGS"} "$@"
-      break;
+      idx-update ${helpargs:+"$helpargs"} "$@"
+      break
       ;;
     reset-master)
       shift
-      idx-reset-master ${HELPARGS:+"$HELPARGS"} "$@"
-      break;
+      idx-reset-master ${helpargs:+"$helpargs"} "$@"
+      break
+      ;;
+    branch)
+      shift
+      idx-branch ${helpargs:+"$helpargs"} "$@"
+      break
       ;;
     --) # end of all options
       shift
@@ -380,35 +573,50 @@ idx() {
 }
 
 _idx() {
-  local cur prev opts allopts
-  COMPREPLY=()
-  cur="${COMP_WORDS[COMP_CWORD]}"
-  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  # https://tylerthrailkill.com/2019-01-19/writing-bash-completion-script-with-subcommands/
+  local i cmd
+  i=1
 
-  opts="$IDX_COMMANDS $IDX_OPTIONS"
-  allopts="all ${opts}"
+  # find previous command (rather than option)
+  while [[ "$i" -lt "$COMP_CWORD" ]]; do
+    local s
+    s="${COMP_WORDS[i]}"
+    case "$s" in
+      -*)
+        ;;
+      *)
+        cmd="$s"
+        break
+        ;;
+    esac
+    (( i++ ))
+  done
 
-  #
-  #  Complete the arguments to some of the basic commands.
-  #
-  case "${prev}" in
+  if [[ "$i" -eq "$COMP_CWORD" ]]; then
+    # 'idx'
+    local cur
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    COMPREPLY=($(compgen -W "all $IDX_COMMANDS $IDX_OPTIONS" -- "${cur}"))
+    return
+  fi
+
+  case "$cmd" in
     all)
-      # shellcheck disable=SC2207
-      COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
-      return 0;
+      _idx-all
+      return 0
       ;;
     update)
-      # shellcheck disable=SC2207
-      COMPREPLY=( $(compgen -W "${IDX_UPDATE_OPTIONS}" -- ${cur}) )
-      return 0;
+      _idx-update
+      return 0
+      ;;
+    branch)
+      _idx-branch
+      return 0
       ;;
     *)
     ;;
   esac
 
-  # shellcheck disable=SC2207
-  COMPREPLY=($(compgen -W "${allopts}" -- ${cur}))
-  return 0
 }
 
 # Register the completion function
